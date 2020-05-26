a7d58a29ed58fc5ec22ca527298c3a3277530b58
Tbetcha <troy.boettger@gmail.com>
2020-05-26 05:35:24 -0400

    refactor to use async await

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 const http = new SimpleHTTP();
 
-// //get users
-// http
-// 	.get('https://jsonplaceholder.typicode.com/users')
-// 	.then((data) => console.log(data))
-// 	.catch((err) => console.log(err));
+//get users
+http
+	.get('https://jsonplaceholder.typicode.com/users')
+	.then((data) => console.log(data))
+	.catch((err) => console.log(err));
 
 //user data
 const data = {
@@ -15,17 +15,17 @@ const data = {
 	email: 'jdoe@mailme.com',
 };
 
-// // //create user
-// http
-// 	.post('https://jsonplaceholder.typicode.com/users')
-// 	.then((data) => console.log(data))
-// 	.catch((err) => console.log(err));
+// //create user
+http
+	.post('https://jsonplaceholder.typicode.com/users')
+	.then((data) => console.log(data))
+	.catch((err) => console.log(err));
 
-// //update user
-// http
-// 	.put('https://jsonplaceholder.typicode.com/users/2', data)
-// 	.then((data) => console.log(data))
-// 	.catch((err) => console.log(err));
+//update user
+http
+	.put('https://jsonplaceholder.typicode.com/users/2', data)
+	.then((data) => console.log(data))
+	.catch((err) => console.log(err));
 
 // //http delete
 
